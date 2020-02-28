a6068d31ca241d0f48a1e9ff52417eb68cc5dfab
robinsor1044 <61600533+robinsor1044@users.noreply.github.com>
2020-02-28 14:45:06 -0500

    first commit

---
+++ P2T1_salesPrediction_RobinsonRonald.py
@@ -0,0 +1,21 @@
+
+
+# created a variable call total sales
+# calculated the profit precentage of total sales
+# displayed the profit
+# print the dollar format
+#
+# February 25, 2020
+# CTI-110 P2T1 - Sales Prediction
+# Ronald Robinson
+
+
+# Get the projected total sales.
+total_sales = float (input('Enter the projected sales: '))
+
+#Calculate the profit of 23 precent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ('The profit is $', format (profit, ',.2f'))
+
